82fb30ba37611b155addb526bcb697e7e2ce4101
Natalie Gillam <ngillam@hotmail.co.uk>
2025-02-27 21:55:39 +0000

    added new words

--- js/questions.js
+++ js/questions.js
@@ -1,19 +1,55 @@
 const questions = [
   {
     id: 1,
-    q: "",
-    definition: `
-`,
+    q: "Still Image",
+    definition:
+      "When a group freezes in a position which is like a photograph.",
     options: ["yes", "no"],
     a: 0,
     category: "unknown",
   },
-]
+  {
+    id: 2,
+    q: "Semiotics",
+    definition:
+      "Symbols created on stage to give symbolic meaning (using actors, set, lighting, costume and props).",
+    options: ["yes", "no"],
+    a: 0,
+    category: "unknown",
+  },
+  {
+    id: 3,
+    q: "Proxemics",
+    definition:
+      "The positioning and distance of characters on stage to give dramatic impact.",
+    options: ["yes", "no"],
+    a: 0,
+    category: "unknown",
+  },
+  {
+    id: 4,
+    q: "Mime",
+    definition:
+      "Acting without words, using only gesture, facial expression and movement.",
+    options: ["yes", "no"],
+    a: 0,
+    category: "unknown",
+  },
+  {
+    id: 5,
+    q: "Improvisation",
+    definition:
+      "Making up a scene in order to explore a situation or relationship.",
+    options: ["yes", "no"],
+    a: 0,
+    category: "unknown",
+  },
+];
 
 // Improvisation
-// Character Profile  
-// Character  
-// Facial Expression 
-// Body Language   
-// Vocals  
+// Character Profile
+// Character
+// Facial Expression
+// Body Language
+// Vocals
 // Thought Tracking
