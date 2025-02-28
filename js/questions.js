82364c5e60f121569b8aa800f4101248d668dca7
Natalie Gillam <ngillam@hotmail.co.uk>
2025-02-28 08:54:48 +0000

    added new questions

--- js/questions.js
+++ js/questions.js
@@ -44,6 +44,59 @@ const questions = [
     a: 0,
     category: ["unknown", "drama techniques"],
   },
+  {
+    id: 6,
+    q: "Facial expression",
+    definition: "The feelings expressed on a person's face",
+    options: ["yes", "no"],
+    a: 0,
+    category: ["unknown", "performance skills"],
+  },
+  {
+    id: 7,
+    q: "Physicality / body language",
+    definition:
+      "The way in which a person communicates emotion, using gestures, movements and mannerisms. ",
+    options: ["yes", "no"],
+    a: 0,
+    category: ["unknown", "performance skills"],
+  },
+  {
+    id: 8,
+    q: "Vocals",
+    definition:
+      "The way in which the voice is used to present emotion using tone (emotion) / pitch (high or low) / pace (speed) / volume (loud or quiet) / accent (region). ",
+    options: ["yes", "no"],
+    a: 0,
+    category: ["unknown", "performance skills"],
+  },
+  {
+    id: 9,
+    q: "space",
+    definition:
+      "The area between and around the actors / the theatre space in which the drama takes place.",
+    options: ["yes", "no"],
+    a: 0,
+    category: ["unknown", "performance skills"],
+  },
+  {
+    id: 10,
+    q: "levels",
+    definition:
+      "The use of different heights on stage to show how high or low status a character is.",
+    options: ["yes", "no"],
+    a: 0,
+    category: ["unknown", "performance skills"],
+  },
+  {
+    id: 11,
+    q: "tension",
+    definition:
+      "when the body feels tense / The concept of making the audience want to know what happens next.",
+    options: ["yes", "no"],
+    a: 0,
+    category: ["unknown", "performance skills"],
+  },
 ];
 
 // Character Profile
